9ea75650fddd32a94d385a46471c817318ffcaf7
Zafuzi <zacharyfoutz@gmail.com>
2017-08-21 01:08:54 -0700

    Added basic ability to store data in JSON file and fetch list of articles by ID The real important update on this one is that I learned how to add a subobject to an ObjectType, this is going to be really useful later on My next step would be to find a way to store each list of articles as a json list, either on demand, or polled eveery 20min to and hour

--- schema.js
+++ schema.js
@@ -1,161 +1,206 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull
-} = require('graphql')
+        GraphQLObjectType,
+        GraphQLString,
+        GraphQLInt,
+        GraphQLSchema,
+        GraphQLList,
+        GraphQLNonNull
+    } = require('graphql')
 
-const axios = require('axios'),
-    redis = require('redis'),
-    client = redis.createClient()
+    const axios = require('axios'),
+        redis = require('redis'),
+        client = redis.createClient()
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
+    client.on("error", function (err) {
+        console.log("Error " + err);
+    });
 
-// Customer Type
-const CustomerType = new GraphQLObjectType({
-    name: 'Customer',
-    description: 'Each customer represented in our database used by the test application.',
-    fields: () => ({
-        id: {
-            type: GraphQLString
-        },
-        name: {
-            type: GraphQLString
-        },
-        email: {
-            type: GraphQLString
-        },
-        age: {
-            type: GraphQLInt
-        }
-    })
-})
-
-const SourceType = new GraphQLObjectType({
-    name: 'SourceType',
-    fields: () => ({
-        id: {
-            type: GraphQLString
-        },
-        name: {
-            type: GraphQLString
-        },
-        description: {
-            type: GraphQLString
-        },
-        url: {
-            type: GraphQLString
-        },
-        category: {
-            type: GraphQLString
-        },
-        language: {
-            type: GraphQLString
-        },
-        country: {
-            type: GraphQLString
-        },
-        urlsToLogos: {
-            type: GraphQLString
-        },
-        sortsByAvailables: {
-            type: GraphQLString
-        }
-    })
-})
-
-function fetchSources() {
-
-}
-
-// Root Query
-const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    description: "Root Query is the default query to load customer data",
-    fields: {
-        customer: {
-            type: CustomerType,
-            args: {
-                id: {
-                    type: GraphQLString
-                }
+    // Customer Type
+    const CustomerType = new GraphQLObjectType({
+        name: 'Customer',
+        description: 'Each customer represented in our database used by the test application.',
+        fields: () => ({
+            id: {
+                type: GraphQLString
             },
-            resolve(parentValue, args) {
-                return axios.get('http://localhost:3000/customers/' + args.id)
-                    .then(res => res.data)
+            name: {
+                type: GraphQLString
+            },
+            email: {
+                type: GraphQLString
+            },
+            age: {
+                type: GraphQLInt
             }
-        },
-        customers: {
-            type: new GraphQLList(CustomerType),
-            resolve(parentValue, args) {
-                return axios.get('http://localhost:3000/customers/')
-                    .then(res => res.data)
+        })
+    })
+
+    const SourceType = new GraphQLObjectType({
+        name: 'SourceType',
+        fields: () => ({
+            id: {
+                type: GraphQLString
+            },
+            name: {
+                type: GraphQLString
+            },
+            description: {
+                type: GraphQLString
+            },
+            url: {
+                type: GraphQLString
+            },
+            category: {
+                type: GraphQLString
+            },
+            language: {
+                type: GraphQLString
+            },
+            country: {
+                type: GraphQLString
+            },
+            urlsToLogos: {
+                type: GraphQLString
+            },
+            sortsByAvailables: {
+                type: GraphQLString
             }
-        },
-        sources: {
-            type: new GraphQLList(SourceType),
-            resolve(parentValue, args) {
-                client.get('sources', (err, reply) => {
-                    if (err == null) {
-                        return reply
-                    } else {
-                        return null
+        })
+    })
+
+    const ArticleType = new GraphQLObjectType({
+        name: 'Article',
+        fields: () => ({
+            author: {
+                type: GraphQLString
+            },
+            title: {
+                type: GraphQLString
+            },
+            description: {
+                type: GraphQLString
+            },
+            url: {
+                type: GraphQLString
+            },
+            urlToImage: {
+                type: GraphQLString
+            },
+            publishedAt: {
+                type: GraphQLString
+            }
+        })
+    })
+
+    const ArticleListType = new GraphQLObjectType({
+        name: 'ArticleList',
+        fields: () => ({
+            status: {
+                type: GraphQLString
+            },
+            source: {
+                type: GraphQLString
+            },
+            sortBy: {
+                type: GraphQLString
+            },
+            articles: {
+                type: new GraphQLList(ArticleType)
+            }
+        })
+    })
+
+    // Root Query
+    const RootQuery = new GraphQLObjectType({
+        name: 'RootQueryType',
+        description: "Root Query is the default query to load customer data",
+        fields: {
+            customer: {
+                type: CustomerType,
+                args: {
+                    id: {
+                        type: GraphQLString
                     }
-                })
-                return fetchSources()
-            }
-        }
-    }
-})
-
-
-
-// Mutations
-const mutation = new GraphQLObjectType({
-    name: 'Mutation',
-    fields: {
-        addCustomer: {
-            type: CustomerType,
-            args: {
-                name: {
-                    type: new GraphQLNonNull(GraphQLString)
                 },
-                email: {
-                    type: new GraphQLNonNull(GraphQLString)
-                },
-                age: {
-                    type: new GraphQLNonNull(GraphQLInt)
+                resolve(parentValue, args) {
+                    return axios
+                        .get('http://localhost:3000/customers/' + args.id)
+                        .then(res => res.data)
                 }
             },
-            resolve(parentValue, args) {
-                return axios.post('http://localhost:3000/customers', {
+            customers: {
+                type: new GraphQLList(CustomerType),
+                resolve(parentValue, args) {
+                    return axios
+                        .get('http://localhost:3000/customers/')
+                        .then(res => res.data)
+                }
+            },
+            sources: {
+                type: new GraphQLList(SourceType),
+                resolve: async(parentValue, args) => {
+                    return axios
+                        .get('http://localhost:3000/sources/')
+                        .then(res => res.data)
+                }
+            },
+            article: {
+                type: ArticleListType,
+                args: {
+                    id: {
+                        type: GraphQLString
+                    }
+                },
+                resolve(parentValue, args) {
+                    return axios
+                        .get('https://newsapi.org/v1/articles?apiKey=ccfdc66609fc4b7b87258020b85d4380&source=' + args.id)
+                        .then(res => res.data)
+                }
+            }
+        }
+    })
+
+    // Mutations
+    const mutation = new GraphQLObjectType({
+        name: 'Mutation',
+        fields: {
+            addCustomer: {
+                type: CustomerType,
+                args: {
+                    name: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    email: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    age: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    }
+                },
+                resolve(parentValue, args) {
+                    return axios
+                        .post('http://localhost:3000/customers', {
                         name: args.name,
                         email: args.email,
                         age: args.age
                     })
-                    .then(res => res.data)
-            }
-        },
-        deleteCustomer: {
-            type: CustomerType,
-            args: {
-                id: {
-                    type: GraphQLString
+                        .then(res => res.data)
                 }
             },
-            resolve(parentValue, args) {
-                return axios.delete('http://localhost:3000/customers/' + args.id)
-                    .then(res => res.data)
+            deleteCustomer: {
+                type: CustomerType,
+                args: {
+                    id: {
+                        type: GraphQLString
+                    }
+                },
+                resolve(parentValue, args) {
+                    return axios
+                        .delete('http://localhost:3000/customers/' + args.id)
+                        .then(res => res.data)
+                }
             }
         }
-    }
-})
+    })
 
-module.exports = new GraphQLSchema({
-    query: RootQuery,
-    mutation
-})
+    module.exports = new GraphQLSchema({query: RootQuery, mutation})
--- server.js
+++ server.js
@@ -4,18 +4,32 @@ const express = require('express'),
     app = express(),
     axios = require('axios'),
     redis = require('redis'),
-    client = redis.createClient()
+    client = redis.createClient(),
+    fs = require('fs')
 
-app.use('/graphql', expressGraphQL({
-    schema: schema,
-    graphiql: true
-}))
+app.use('/graphql', expressGraphQL({schema: schema, graphiql: true}))
+
+// var jsonServer = require('json-server') var server = jsonServer.create()
+// server.use(jsonServer.defaults) server.use('/customers',
+// jsonServer.router('customers.json')) server.use('/sources',
+// jsonServer.router('sources.json')) server.listen(3000)
 
 app.listen(4000, () => {
-    axios.get('https://newsapi.org/v1/sources?language=en')
-        .then(res => {
-            //console.log(res.data.sources);
-            client.set('sources', JSON.stringify(res.data.sources))
-        })
+    process.once('SIGUSR2', () => {
+        axios
+            .get('https://newsapi.org/v1/sources?language=en')
+            .then(res => {
+                let sources = res.data.sources
+                let data = {
+                    "sources": []
+                }
+                for (let i = 0; i < sources.length; i++) {
+                    console.log("I: " + i)
+                    data["sources"].push(sources[i])
+                }
+                fs.writeFile("./sources.json", JSON.stringify(data))
+            })
+    })
+
     console.log('Server is listening at localhost:4000')
 })
